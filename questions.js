eb341422f7151676d0cb647051f5dde603ebd410
Brianne <brianne.ewell1@gmail.com>
2020-11-14 16:19:27 -0700

    adding name and email

--- questions.js
+++ questions.js
@@ -1,2 +1,15 @@
 const questions = [
-	
+    {
+		//Author
+		type: 'input',
+		message: 'Enter your first and last name',
+		name: 'author',
+		validate: function(name) {
+			let pass = name.match(/^[a-zA-Z]+ [a-zA-Z]+$/g);
+			if (pass) {
+				return true;
+			}
+
+			return 'Please enter a valid first and last name.';
+		},
+	},
