d12f9893d56ddf8298a4a549dd66cc1d34736cd8
Brianne <brianne.ewell1@gmail.com>
2020-11-14 16:26:12 -0700

    adding space to enter extra links

--- questions.js
+++ questions.js
@@ -60,4 +60,24 @@ const questions = [
 		message: 'Would you like to add additional project links?',
 		default: false,
 	},
+	{
+		// Get additional project links
+		type: 'input',
+		name: 'projectLinks',
+		message: 'Enter the additional project link(s) using the entire link, including the http(s):. (* Use comma "," to seprate each link)',
+		when: function(answers) {
+			return answers.isLinks !== false;
+		},
+		validate: function(projectLinks) {
+			for (let i = 0; i < projectLinks.split(',').length; i++) {
+				let pass = projectLinks.split(',')[i].trim().match(/^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$/g);
+			
+			if (pass) {
+				return true;
+			}
+				
+			return 'Provide the valid links of the project using the entire link, including the http(s):';
+			}
+		},
+	},
 	
