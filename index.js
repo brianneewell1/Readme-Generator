9ed3693226e9e8ec20a1bd30d4ee4291bb73b30d
Brianne Ewell <brianne.ewell1@gmail.com>
2020-11-14 16:07:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ function userQ() {
 	return prompt(questions);
 }
 
+// Use async
+async function init() {
+  //try
+	try {
+    const answers = await userQ();
+    
+    console.log(JSON.stringify(answers, null, '\t'));
+
+		const readme = template(answers);
+
+    await writeFile('README.md', readme);
+
+		console.log('README.md is complete!');
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+init();
