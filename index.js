cf36c8b50a8b612dd1e9b8d63f63a534b4aee276
Brianne Ewell <brianne.ewell1@gmail.com>
2020-11-14 16:00:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 const { prompt } = require('inquirer');
 const { writeFile } = require('fs');
 const { promisify } = require('util');
+
+//Import questions.js 
+const questions = require('./lib/questions')
+
+// Import template.js
+const template = require('./lib/template')
+
+// Wrap fs.writeFile inside util.promisify
+const writeFile = promisify(writeFile);
