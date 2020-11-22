5290942d7a293e5d1e6afa3908bf2677989575ed
Brianne <brianne.ewell1@gmail.com>
2020-11-21 20:07:25 -0700

    updating inquirer and fs variables

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 //Global Variables
 //Import inquirer and node modules
-const { prompt } = require('inquirer');
-const { writeFile } = require('fs');
-const { promisify } = require('util');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 //Import questions.js 
 const questions = require('./lib/questions')
 
 // Import template.js
 const template = require('./lib/template')
-
-// Wrap fs.writeFile inside util.promisify
-const writeFile = promisify(writeFile);
 
 // First Message
 console.log('This is a Github README generator');
